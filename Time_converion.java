5b3e9a48a94efd4b1c8004fc44b57da8d3e3750f
Hemanth Vasu <hemanthvasu41468@gmail.com>
2026-05-18 21:24:11 +0530

    Time conversion

--- Time_converion.java
+++ Time_converion.java
@@ -1,3 +1,29 @@
 public class Time_converion {
-    
+     public static void main(String[] args) {
+
+        String s = "07:05:45PM";
+
+        System.out.println(timeConversion(s));
+    }
+
+    public static String timeConversion(String s) {
+            
+        int hour=Integer.parseInt(s.substring(0,2));
+
+        String ampm=s.substring(8,10);
+
+        if(ampm.equals("PM") && hour !=12)
+        {
+            hour+=12;
+        }
+
+        if(ampm.equals("AM") && hour==12)
+        {
+            hour=0;
+        }
+
+        String hh=String.format("%02d",hour);
+
+        return hh+s.substring(2, )
+    }
 }
